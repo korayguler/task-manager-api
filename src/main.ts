59bbfeb75130d1d483f079e1942f73095a8d6633
Koray Guler <us.koray.guler@gmail.com>
2021-10-21 22:38:25 +0300

    Project inject configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import mongoConnect from '@services/mongo'
 import express, { Application } from 'express'
 import env from 'dotenv'
-import { HomeRouter } from './routes'
+import notFound from '@middleware/not-found'
+import errorHandler from '@middleware/error-handler'
+import TaskRouter from '@routes/tasks'
 
 //.env
 env.config()
@@ -8,11 +11,27 @@ env.config()
 //express
 const app: Application = express()
 
+app.use(express.json() as express.RequestHandler)
+app.use(express.urlencoded() as express.RequestHandler)
+
+app.use('/api/v1/tasks', TaskRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 1337
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost'
 
-app.use('/api/v1/tasks', HomeRouter)
+const runServer = async () => {
+  try {
+    await mongoConnect(process.env.MONGODB_URI || 'connection-string')
 
-app.listen(PORT, () => {
-  console.info(`Service starting at ${SERVER_URL}:${PORT}`)
-})
+    app.listen(PORT, () => {
+      console.info(`Service starting at ${SERVER_URL}:${PORT}`)
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+runServer()
