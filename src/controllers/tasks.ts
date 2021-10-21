b67d69ee9ac74aad538dca65c30e30aedfdf90c4
Koray Guler <us.koray.guler@gmail.com>
2021-10-21 22:33:54 +0300

    Task Controllers create

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,23 +1,58 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
-const getTask = (req: Request, res: Response) => {
-  res.send('getTask')
-}
+import asyncHandler from '@middleware/async-handler'
 
-const getTasks = (req: Request, res: Response) => {
-  res.send('getTasks')
-}
+import createCustomError from '@errors/custom-error'
 
-const createTask = (req: Request, res: Response) => {
-  res.send('createTask')
-}
+import Task from '@models/task'
 
-const updateTask = (req: Request, res: Response) => {
-  res.send('updateTask')
-}
+const getTask = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const task = await Task.findById(req.params.id)
 
-const deleteTask = (req: Request, res: Response) => {
-  res.send('deleteTask')
-}
+    if (!task) {
+      return next(createCustomError('Not Found', 404))
+    }
+
+    res.status(200).json({ status: 'Success', data: task })
+  },
+)
+
+const getTasks = asyncHandler(async (req: Request, res: Response) => {
+  const tasks = await Task.find()
+  res.status(200).json({ status: 'Success', data: tasks })
+})
+
+const createTask = asyncHandler(async (req: Request, res: Response) => {
+  const task = await Task.create(req.body)
+  res.status(200).json({ status: 'Success', data: task })
+})
+
+const updateTask = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const task = await Task.findOneAndUpdate({ id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!task) {
+      return next(createCustomError('Not Found', 404))
+    }
+
+    res.status(200).json({ status: 'Success', data: task })
+  },
+)
+
+const deleteTask = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const task = await Task.findByIdAndDelete(req.params.id)
+
+    if (!task) {
+      return next(createCustomError('Not Found', 404))
+    }
+
+    res.status(200).json({ status: 'Success', data: task })
+  },
+)
 
 export { createTask, getTask, getTasks, updateTask, deleteTask }
